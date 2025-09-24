1a856d8196d16fa39892edbd85648b9a4f6704c2
CLOE INTIHAR <cloeintihar24@gmail.com>
2025-09-24 11:04:22 +0200

    creation classe

---
+++ SalleDeClasse.py
@@ -0,0 +1,15 @@
+
+class SalleDeClasse :
+    def __init__(self, id_salle : str, capacite : int):
+        self.id_salle = id_salle
+        self.capacite = capacite 
+        
+    def __str__(self):
+        return f"Salle {self.id_salle} (capacité: {self.capacite} places)"
+    
+if __name__=="__main__" :
+    salle1 = SalleDeClasse("A101", 30)
+    salle2 = SalleDeClasse("B206", 25)
+    salle3 = SalleDeClasse("C304", 25)
+    print(salle2)
+    
