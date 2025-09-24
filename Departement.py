c05576eceb8838b3605a4b14fa00ada3766a29a3
Tote <thomas.pelhatre@gmail.com>
2025-09-24 10:57:45 +0200

    Creating Departement class and a few methods

---
+++ Departement.py
@@ -0,0 +1,61 @@
+from typing import List,Optional
+from Enseignant import Enseignant
+from Matiere import Matiere
+
+class Departement:
+    def __init__(self,nom : str, responsable : Optional[Enseignant] =None, listeEnseignants : List[Enseignant]=[],listeMatieres : List[Matiere] =[] ):
+        """
+        Constructeur de la classe Departement avec comme attributs :
+        -nom (str) : Le nom du département
+        -responsable (Enseignant) : L'enseignant responsable du  département
+        -listeEnseignants (List[Enseignant]) : Liste des enseignants du département
+        -listeMatiere (List[Matiere]) : Liste des matières du département 
+        """
+        self.nom=nom
+        self.responsable =responsable
+        self.listeEnseignants=listeEnseignants
+        self.listeMatieres=listeMatieres
+
+    def __str__(self):
+        return f"Département : {self.nom} avec comme responsable: {self.responsable}"
+    
+    def ajouterEnseignant(self,enseignant : Enseignant):
+        """
+        Ajoute un enseignant à la liste des enseignants du départment
+        """
+        self.listeEnseignants.append(enseignant)
+    
+    def ajouterMatiere(self,matiere : Matiere):
+        """
+        Ajoute une matière à la liste des matière du département
+        """
+        self.listeMatieres.append(matiere)
+    
+    def changerResponsable(self,nouveau_responsable : Enseignant):
+        """
+        Change le responsable du département et l'ajoute à la liste des enseignants si il n'en faisait pas partie
+        """
+        self.responsable = nouveau_responsable
+        if not nouveau_responsable in self.listeEnseignants:
+            self.listeEnseignants.append(nouveau_responsable)
+    
+    def enleverEnseignant(self,enseignant_partant: Enseignant):
+        """
+        Enlève un enseignant de la liste des enseignants en vérifiant auparavant si il faisait bien partie du département
+        """
+        if not enseignant_partant in self.listeEnseignants:
+            return "Cet enseignant ne fait pas partie du département"
+        self.listeEnseignants.remove(enseignant_partant)
+
+    def enleverMatiere(self,matiere_partante: Matiere):
+        """
+        Enlève une matière de la liste des matières en vériafiant auparavant si elle faisait bien partie du département
+        """
+        if not matiere_partante in self.listeMatieres:
+            return "Cette matière ne fait pas partie du département"
+        self.listeMatieres.remove(matiere_partante)
+
+
+
+
+        
