de9ad66ac208be637cd5e1d7e1c8888e5553e8fc
rraanto <ranto.rakotondrajoa@gmail.com>
2025-11-19 17:49:43 +0100

    Mise à jour de la classe pour suivre le diagramme UML semaine 47: - Mise à jour des noms de variables et méthodes (Classe.aCamelCase, Classe.CamelCase(les_params) - Implémentation de la classe Page() pour représenter les pages - Implémentation du type énuméré StatutPage - Ajout de tests

--- SiteWeb.py
+++ SiteWeb.py
@@ -1,75 +1,156 @@
 from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+class Page:
+    """
+    Classe représentant une page dans un site web
+    """
+
+    def __init__(self, titre: str="", contenu: str=""):
+        """
+        :url: Adresse de la page web
+        :titre: Titre de la page
+        :contenu: Contenu de la page
+        """
+        self.aContenu = contenu
+        self.aTitre = titre
+
+    def ajouterContenu(self, nouveau_contenu: str):
+        self.aContenu += nouveau_contenu
+
+    def __str__(self) -> str:
+        out = f"Page {self.aTitre if self.aTitre != "" else 'sans titre'}: "
+        if self.aContenu != "":
+            out += f"\nContenu: \n{self.aContenu}"
+        return out
+
+class StatutPage(str, Enum):
+    """
+    Typage des statuts possible d'une page
+    """
+    ACTIF = 'ACTIF'
+    EN_MAINTENANCE = 'EN_MAINTENANCE'
+    FERME = 'FERME'
+
+    def __str__(self):
+        return self.name.replace('_', ' ').capitalize()
+
 
 class SiteWeb:
     """
     Classe représentant le site Internet d'un collège.
     """
 
-    def __init__(self, Url, Date_Creation, Responsable, Statut="actif", Pages=None):
+    def __init__(self, url:str, date_creation: datetime , statut=StatutPage.ACTIF, pages: Optional[list[Page]] = None):
         """
         Initialise un site web.
         
         : url: L’adresse du site 
         : date_creation: Date de création du site
-        : responsable: Nom de la personne responsable du site 
         : statut: Statut du site 
-        : pages: Liste des pages disponibles sur le site
+        : pages: dictionnaire de pages indexés par leurs url présentes sur le site
         """
-        self.url = Url
-        self.date_creation = Date_Creation
-        self.responsable = Responsable
-        self.statut = Statut
-        self.pages = Pages if Pages else []
+        self.aUrl = url
+        self.aDateCreation = date_creation
+        self.aPages = {}
+
+        # Ajout un à un des pages pour avoir les bons urls
+        if pages is not None:
+            for page in pages:
+                self.ajouterPage(page.aTitre, page.aContenu)
+
+        if statut in StatutPage.__members__:
+            self.aStatut = statut
+        else:
+            print(f"Attention: Statut {statut} inconnu lors de l'initialisation, valeur {StatutPage.ACTIF} utilisé")
+            self.aStatut = StatutPage.ACTIF
 
     # --- Méthodes de gestion des pages ---
-    def ajouter_page(self, Titre, Contenu):
+    def ajouterPage(self, titre:str, contenu: str=""):
         """
         Ajoute une nouvelle page au site web.
+        :titre: titre de la page à ajouter (doit être unique sur le site)
         """
-        page = {"titre": Titre, "contenu": Contenu, "date_ajout": datetime.now()}
-        self.pages.append(page)
-        print(f" Page '{Titre}' ajoutée au site {self.url}.")
+        url_nouvelle_page = f"{self.aUrl}/{titre.replace(' ', '-').lower()}"
+        if url_nouvelle_page not in self.aPages:
+            self.aPages.setdefault(url_nouvelle_page, Page(titre=titre, contenu=contenu))
+        else:
+            print(f"Attention: tentative d'ajout de page échouée, la page {url_nouvelle_page} existe déjà")
 
-    def supprimer_page(self, Titre):
+
+    def supprimerPage(self, titre: str):
         """
         Supprime une page du site à partir de son titre.
         """
-        for page in self.pages:
-            if page["titre"] == Titre:
-                self.pages.remove(page)
-                print(f" Page '{Titre}' supprimée du site {self.url}.")
-                return
-        print(f" Aucune page nommée '{Titre}' trouvée sur le site.")
+        url_page = f"{self.aUrl}/{titre.replace(' ', '-').lower()}"
+        if url_page in self.aPages:
+            del(self.aPages[url_page])
+        else:
+            print(f"Attention: tentative de suppression échouée: page {url_page} inconnue")
 
-    def afficher_pages(self):
+    def afficherPages(self):
         """
         Affiche la liste des pages disponibles sur le site.
         """
-        if not self.pages:
+        if len(self.aPages) > 0:
+            print("Pages disponibles sur le site:".center(50, '-'))
+            for url in self.aPages:
+                print(url + ":")
+                print(self.aPages[url])
+        else:
             print("Aucune page n’est disponible pour le moment.")
-        else:
-            print(f" Pages du site {self.url} :")
-            for page in self.pages:
-                print(f" - {page['titre']} (créée le {page['date_ajout'].strftime('%d/%m/%Y')})")
-
+        
     # --- Méthodes d'administration ---
-    def changer_statut(self, Nouveau_Statut):
+    def changerStatut(self, nouveau_statut: str):
         """
-        Change le statut du site (actif, maintenance, fermé).
+        Change le statut du site (ACTIF, EN_MAINTENANCE, FERME).
         """
-        if Nouveau_Statut in ["actif", "maintenance", "fermé"]:
-            self.statut = Nouveau_Statut
-            print(f" Le site {self.url} est maintenant en mode '{self.statut}'.")
+        if nouveau_statut in StatutPage.__members__:
+            self.aStatut = nouveau_statut
+            print(f" Le site {self.aUrl} est maintenant en mode '{self.aStatut}'.")
         else:
-            print(" Statut invalide. Valeurs possibles : actif, maintenance, fermé.")
+            print(" Statut invalide. Valeurs possibles : ACTIF, EN_MAINTENANCE, FERME")
 
-    def afficher_infos(self):
+    def afficherInfos(self):
         """
         Affiche toutes les informations du site web.
         """
-        print(f"\n Informations sur le site :")
-        print(f" - URL : {self.url}")
-        print(f" - Responsable : {self.responsable}")
-        print(f" - Date de création : {self.date_creation.strftime('%d/%m/%Y')}")
-        print(f" - Statut : {self.statut}")
-        print(f" - Nombre de pages : {len(self.pages)}")
+        print(f"Informations sur le site :".center(50, '-'))
+        print(f" - URL : {self.aUrl}")
+        print(f" - Date de création : {self.aDateCreation.strftime('%d/%m/%Y')}")
+        print(f" - Statut : {self.aStatut}")
+        print(f" - Nombre de pages : {len(self.aPages)}")
+
+
+"""
+Tests
+"""
+if __name__ == "__main__":
+
+    page_predefinie = Page(titre="Morphisme de Hurewicz", contenu = "Ab(pi_1(X)) = H_1(X)")
+    print("Test page unique: \n" + page_predefinie.__str__())
+
+    site = SiteWeb(
+        url="https://M2MMAA.com",
+        date_creation=datetime.now(),
+        pages = [page_predefinie]
+    )
+
+    site.afficherInfos()
+
+    nouveau_contenu = "Bonjour à tous.tes, je vous informe que demain vous n'avez pas cours!!"
+    site.ajouterPage(titre="Annonce de la responsable", contenu=nouveau_contenu)
+
+    site.afficherPages()
+
+    # Suppression de page inexistante
+    site.supprimerPage("une page inexistante")
+
+    # Suppression de page existante
+    site.supprimerPage("annonce de la responsable")
+    site.afficherPages()
+
+    # Ajout de page déjà existante
+    site.ajouterPage(titre="Morphisme de hurewicz", contenu="pi_n(X) s'injecte dans H_n(X)")
+    site.afficherPages()
